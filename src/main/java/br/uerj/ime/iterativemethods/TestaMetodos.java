21ec76c199e3f32beecd7b3dd308f506a5945e10
Renato Viegas <renato.viegas@dataprev.gov.br>
2019-12-16 20:46:29 -0300

    Algoritmo GaussSeidel

--- src/main/java/br/uerj/ime/iterativemethods/TestaMetodos.java
+++ src/main/java/br/uerj/ime/iterativemethods/TestaMetodos.java
@@ -1,5 +1,6 @@
 package br.uerj.ime.iterativemethods;
 
+import br.uerj.ime.iterativemethods.classes.GaussSeidel;
 import br.uerj.ime.iterativemethods.classes.Jacobi;
 import br.uerj.ime.iterativemethods.classes.Matrix;
 import br.uerj.ime.iterativemethods.interfaces.IterativeMethods;
@@ -7,29 +8,19 @@ import br.uerj.ime.iterativemethods.interfaces.IterativeMethods;
 public class TestaMetodos {
 
 	public static void main(String[] args) {
-		
-		double precisao = 0.01;
-		double matriz[][] = new double[3][3];
-		double constants[] = new double[3];
 
-		matriz[0][0] = 10;
-		matriz[0][1] = 2;
-		matriz[0][2] = 3;
-		matriz[1][0] = 1;
-		matriz[1][1] = 5;
-		matriz[1][2] = 1;
-		matriz[2][0] = 2;
-		matriz[2][1] = 3;
-		matriz[2][2] = 10;
-		
-		constants[0] = 7;
-		constants[1] = -8;
-		constants[2] = 6;
-		
+		double precisao = 0.01;
+		double matriz[][] = { { 10, 2, 3 }, { 1, 5, 1 }, { 2, 3, 10 } };
+		double constants[] = {7, -8, 6};
+
 		Matrix m = new Matrix(matriz, constants);
-		
-		IterativeMethods jacobi = new Jacobi();		
-		jacobi.calculate(m, precisao, 100);
+
+		/*
+		 * IterativeMethods jacobi = new Jacobi(); jacobi.calculate(m, precisao, 100);
+		 */
+
+		IterativeMethods gaussSeidel = new GaussSeidel();
+		gaussSeidel.calculate(m, precisao, 100);
 	}
 
 }
--- src/main/java/br/uerj/ime/iterativemethods/classes/GaussSeidel.java
+++ src/main/java/br/uerj/ime/iterativemethods/classes/GaussSeidel.java
@@ -1,14 +1,97 @@
 package br.uerj.ime.iterativemethods.classes;
 
+import br.uerj.ime.iterativemethods.exception.ElementByMainDiagonalMatrixHasZeroException;
+import br.uerj.ime.iterativemethods.helper.HelperMatrix;
 import br.uerj.ime.iterativemethods.interfaces.IterativeMethods;
 
 public class GaussSeidel implements IterativeMethods {
 
 	@Override
 	public void calculate(Matrix matrix, double precision, int maxIterations) {
-		// TODO Auto-generated method stub
-		
+
+		double A[][] = matrix.getMatrix();
+		double b[] = matrix.getConstants();
+		int n = A.length;
+
+		double previousResult[] = new double[n];
+		double currentResults[] = new double[n];
+		int iterations = 0;
+
+		// valida a matriz
+		int element = HelperMatrix.getElementWithZeroInDiagonalMain(A);
+		if (element > -1) {
+			new ElementByMainDiagonalMatrixHasZeroException(element);
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++)
+				System.out.print("[" + A[i][j] + "]  ");
+			System.out.println("[x" + i + "] = [" + b[i] + "]");
+		}
+
+		System.out.println("");
+		System.out.println("Iniciando o cálculo utilizando o metódo de Gauss-Seidel com precisão: " + precision);
+
+		System.out.println("");
+
+		for (int i = 0; i < n; i++)
+			previousResult[i] = 0;
+
+		while (true) {
+
+			iterations++;
+
+			for (int i = 0; i < n; i++) {
+
+				currentResults[i] = b[i];
+
+				for (int j = 0; j < n; j++) {
+
+					if (i > j) {
+						currentResults[i] -= A[i][j] * currentResults[j];
+					}
+
+					if (i < j) {
+						currentResults[i] -= A[i][j] * previousResult[j];
+					}
+				}
+
+				currentResults[i] /= A[i][i];
+			}
+
+			double error = 0;
+
+			for (int i = 0; i < n; i++) {
+
+				double errorTemp = 0;
+				for (int j = 0; j < n; j++) {
+					errorTemp += A[i][j] * currentResults[j];
+				}
+
+				error += Math.abs(errorTemp - b[i]);
+			}
+
+			if (error < precision) {
+				break;
+			}
+
+			previousResult = currentResults;
+
+		}
+
+		HelperMatrix.print(A);
+		HelperMatrix.print(previousResult);
+		System.out.println(iterations);
+
+		for (double[] a : A) {
+			double errorTemp = 0;
+			for (int j = 0; j < n; j++) {
+				errorTemp += a[j] * previousResult[j];
+			}
+
+			System.out.print(errorTemp + " ");
+		}
+
 	}
 
-	
 }
